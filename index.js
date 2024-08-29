7df79ecd0e7f0423e1b19fbf96d46ef3e5423824
Hecone <angloamir@gmail.com>
2024-08-30 00:15:05 +0200

    Ajouter la base de données d'un chat web avec SQLite3

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { Server } from "socket.io";
 import { createServer } from 'node:http';
+import Database from 'better-sqlite3';
+const db = new Database('chat.db');
+db.prepare(`CREATE TABLE IF NOT EXISTS Chat(pseudo TEXT, message TEXT)`).run();
 
 const app = express();
 const server = createServer(app)
@@ -10,8 +13,15 @@ app.use(express.static("public"));
 
 io.on("connection", (socket) => {
     console.log('user connected');
+    const table = db.prepare('SELECT * FROM Chat').all()
+    
+    for(const message of table) {
+        socket.emit("new message", message);
+    }
 
     socket.on("new message", (args) => {
+        console.log(args)
+        db.prepare('INSERT INTO Chat(pseudo, message) VALUES (?, ?)').run(args.pseudo, args.message)
         socket.broadcast.emit("new message", args);
     });
 
